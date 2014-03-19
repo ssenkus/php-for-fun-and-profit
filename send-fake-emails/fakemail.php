d713a235abf1e78ca3e651add8a9bf77f65d1655
ssenkus <ssenkus@201303-67WCTW1.graphicproducts.local>
2014-03-19 10:15:55 -0700

    send fake emails

---
+++ send-fake-emails/fakemail.php
@@ -0,0 +1,13 @@
+<?php
+$email = $_POST['email'];
+$content = nl2br($_POST['content']);
+$name = $_POST['name'];
+$sender = $_POST['sender'];
+$subject = $_POST['subject'];
+$headers = "From: $name "."<".$sender.">\r\n";
+//add boundary string and mime type specification
+//$headers .= 'MIME-Version: 1.0' . "\r\n";
+//$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+//send the email
+mail($email, $subject, $content, $headers );
+?>
