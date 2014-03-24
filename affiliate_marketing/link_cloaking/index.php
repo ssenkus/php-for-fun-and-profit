2a6c9ef973aab847227d8a64839509992e8ab431
Steve <steven.senkus@yahoo.com>
2014-03-23 18:42:09 -0700

    TODO: get /out/product to redirect to /out/index.php?id=product

--- affiliate_marketing/link_cloaking/index.php
+++ affiliate_marketing/link_cloaking/index.php
@@ -7,8 +7,8 @@
     <body>
         <h1>My page</h1>
         <ul>
-            <li><a href="out/index.php?id=arduino">Affiliate - Avantlink - Arduino</a></li>
-            <li><a href="out/index.php?id=wire">Affiliate - Avantlink - Wire</a></li>
+            <li><a href="out/index.php?id=arduino">Affiliate - Avantlink - Arduino</a> <a href="out/arduino"> Affiliate - Avantlink - Arduino</a></li>
+            <li><a href="out/index.php?id=wire">Affiliate - Avantlink - Wire</a> <a href="out/wire">Affiliate - Avantlink - Wire</a></li>
             <li><a href="out/index.php?id=test">Affiliate - Yoast, another page</a></li>
             <li><a href="out/index.php?id=phpmysql">Affiliate - AMAZON - PHP,MySQL,CSS,JS book</a></li>
             <li><a href="out/index.php?id=default">Affiliate - Example.com</a></li>
